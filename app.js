8fcd6c925c15a6e38dbe456b8c923a25ada67da5
김대건주임 <agent_kim@itfact.co.kr>
2022-11-19 02:43:55 +0900

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const exp = require('constants');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
